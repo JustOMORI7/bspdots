93e997e45d594d9e05a2deb2f0631fc50b92de07
iamGHOST755 <162158604+iamGHOST755@users.noreply.github.com>
2025-06-15 22:04:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,15 @@ else
   echo "paru installed! Installing packages..."
 fi
 
-paru -S polkit-kde-agent sddm bspwm sxhkd polybar picom dunst nitrogen alacritty thunar thunar-volman gvfs gvfs-mtp brightnessctl-bin maim feh betterlockscreen xsetroot qt6ct nwg-look lxappearance bibata-cursor-theme-bin kvantum papirus-icon-theme materia-gtk-theme kvantum-theme-materia rofi playerctl ttf-cascadia-code xdg-user-dirs
+paru -S --noconfirm polkit-kde-agent sddm bspwm sxhkd polybar dunst nitrogen alacritty thunar thunar-volman gvfs gvfs-mtp brightnessctl maim feh betterlockscreen xorg-xsetroot qt6ct nwg-look lxappearance bibata-cursor-theme-bin kvantum papirus-icon-theme materia-gtk-theme kvantum-theme-materia rofi playerctl ttf-cascadia-mono-nerd xdg-user-dirs
+paru -S --noconfirm libev cmake uthash meson ninja libconfig dbus libglvnd libxcb xcb-util-renderutil xcb-util-image pixman
+
+cd ~
+git clone https://github.com/jonaburg/picom
+cd picom
+meson setup --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
 
 xdg-user-dirs-update
 
@@ -34,7 +42,13 @@ echo "QT_QPA_PLATFORMTHEME=qt6ct" | sudo tee -a /etc/environment
 
 sudo systemctl enable sddm
 
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/sxhkd/sxhkdrc
+
 mkdir -p ~/Pictures/wallpapers
 cp ~/bspdots/wallpapers/* ~/Pictures/wallpapers
+
+rm -rf ~/picom
+rm -rf ~/paru-bin
 
 echo "Installation finished. Please reboot your system."
